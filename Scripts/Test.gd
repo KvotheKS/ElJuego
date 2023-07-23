1b780a5bb0be65b9f73624c7ff99b605a1ef84de
gustavo-tomas <guga.tomas10@gmail.com>
2023-07-23 14:55:59 -0300

    Adding enemy spawn and crosshair

--- Scripts/Test.gd
+++ Scripts/Test.gd
@@ -6,13 +6,36 @@ extends Node2D
 # @TODO: Input Maps use snake_case naming to match GODOT's
 
 var GUARDIAN = preload("res://Scenes/Entities/Enemies/SGuardian.tscn")
+var SINGLE_SHOOTER = preload("res://Scenes/Entities/Enemies/SingleShooter.tscn")
+
+const MIN_X = 30
+const MIN_Y = 30
+const MAX_X = 540
+const MAX_Y = 260
+
+var spawnPoints = []
+var enemies = []
+
 func _ready() -> void:
-	
-	for i in range(0):
-		var temp = GUARDIAN.instance()
-		temp.position.x += rand_range(0,100)
-		add_child(temp)
+
+	# Set random seed
+	rand_seed(OS.get_system_time_msecs())
+
+	# Set possible spawn points
+	var top = Vector2(rand_range(MIN_X, MAX_X), MIN_Y)
+	var bottom = Vector2(rand_range(MIN_X, MAX_X), MAX_Y)
+	var left = Vector2(0, rand_range(MIN_Y, MAX_Y))
+	var right = Vector2(MAX_X, rand_range(MIN_Y, MAX_Y))
+	spawnPoints = [top, bottom, left, right]
+
+	# Set possible enemies
+	enemies = [GUARDIAN, SINGLE_SHOOTER]
+
 	pass
+
+func timeout():
+	print("TIMEOUT")
+	return
 
 # Sends player position to the turret
 func _process(_delta):
@@ -27,3 +50,16 @@ func get_player_position():
 		return $Player.position
 	else:
 		return null
+
+# Spawn more enemies
+func _on_SpawnTimer_timeout():
+
+	# Choose a random spawn point
+	var spawnPoint = spawnPoints[randi() % 4]
+
+	# Choose a random enemy to spawn
+	var enemy = enemies[randi() % 2].instance()
+	enemy.position = spawnPoint
+
+	add_child(enemy)
+	return
